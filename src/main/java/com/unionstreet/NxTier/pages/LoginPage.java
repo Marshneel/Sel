bb76002b03b4e6b0911ca8a8df1fad670369e100
UNIONST\rajeshg <rajesh.gurrala@unionstreet.uk.com>
2017-01-23 07:58:19 +0000

    E2ETests only

--- src/main/java/com/unionstreet/NxTier/pages/LoginPage.java
+++ src/main/java/com/unionstreet/NxTier/pages/LoginPage.java
@@ -11,16 +11,18 @@ public class LoginPage {
     ElementUtils utils = new ElementUtils();
 
     public void doLogin() {
-        try {utils.waitForElementVisible(By.id(USENAME_FIELD));
+        try {
+            utils.waitForElementVisible(By.id(USENAME_FIELD));
             utils.sendText(By.id(USENAME_FIELD), utils.getProperty("userName"));
         } catch (Exception e) {
             utils.getLoginPage();
             utils.waitForElementVisible(By.id(USENAME_FIELD));
             utils.sendText(By.id(USENAME_FIELD), utils.getProperty("userName"));
-            utils.sendText(By.id(PASSWORD_FIELD), utils.getProperty("passWord"));
-            utils.clickBtn(By.cssSelector(LOGINBUTTON));
         }
+        utils.sendText(By.id(PASSWORD_FIELD), utils.getProperty("passWord"));
+        utils.clickBtn(By.cssSelector(LOGINBUTTON));
     }
+
 
     public void failedLogin(String username, String password) {
         utils.sendText(By.id(USENAME_FIELD), username);
