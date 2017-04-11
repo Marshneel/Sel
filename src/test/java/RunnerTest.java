3302a7cc6f95d79b53fb458fbd1468c92997a9b0
UNIONST\rajeshg <rajesh.gurrala@unionstreet.uk.com>
2017-04-11 17:20:03 +0100

    contains batch order (txt file upload)

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -3,6 +3,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"html:target/whisper-html-report", "json:target/whisper_report.json", "com.cucumber.listener.ExtentCucumberFormatter:output/report.html"}, tags = {"E2ETests,@smokePack"})
+@CucumberOptions(plugin = {"html:target/whisper-html-report", "json:target/whisper_report.json", "com.cucumber.listener.ExtentCucumberFormatter:output/report.html"}, tags = {"@E2ETests,@smokePack"})
 public class RunnerTest {
 }
