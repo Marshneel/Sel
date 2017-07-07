c7b3347af15532c88588e8c71b7fd7a8bfc6ea1a
UNIONST\rajeshg <rajesh.gurrala@unionstreet.uk.com>
2017-07-07 17:00:30 +0100

    contains NA165

--- src/main/java/com/unionstreet/NxTier/pages/ServiceDesk_LoginIncidentPage.java
+++ src/main/java/com/unionstreet/NxTier/pages/ServiceDesk_LoginIncidentPage.java
@@ -1,10 +1,13 @@
 package com.unionstreet.NxTier.pages;
 
 import com.unionstreet.NxTier.support.ElementUtils;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 
 import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
+
+import static com.unionstreet.NxTier.support.BaseClass.driver;
 
 /**
  * Created by RajeshG on 21/06/2017.
@@ -27,18 +30,29 @@ public class ServiceDesk_LoginIncidentPage {
     private final String PLEASE_SELECT_AN_INCIDENT_TYPE="//span[contains(text(),'Please select an Incident Type')]";
     private final String THE_SUMMARY_MUST_HAVE_NO_MORE_THAN_256_WORDS="//span[contains(text(),'The Summary must be no more than 256 characters')]";
     private final String THE_SUMMARY_CAN_BE_NO_MORE_THAN_80_CHARACTERS="//span[contains(text(),'The Summary must be no more than 80 characters')]";
-    private final String PLEASE_ENTER_A_SUMMARY_FOR_INCIDENT="//span[contains(text(),'Please enter a Summary for this Incident')]";
+    private final String PLEASE_ENTER_A_SUMMARY_FOR_INCIDENT="//span[contains(text(),'Please enter a brief Summary for this Incident')]";
     private final String EDIT_AN_INCIDENT="//h1[contains(text(),'Edit an incident')]";
     private final String PRIORITY_TEXT="//label[contains(text(),'Priority')]";
     private final String PRIORITY_DROPDOWN="IncidentPriority";
     private final String INCIDENT_TYPE="IncidentType";
-    private final String INCIDENT_SYMPTOM="Test Symptom 1";
+    private final String INCIDENT_SYMPTOM="IncidentSymptom";
     private final String INCIDENT_CATEGORY="IncidentCategory";
     private final String INCIDENT_SUMMARY="IncidentSummary";
     private final String INCIDENT_OWNER="IncidentOwner";
     private final String CONTACT_IS_THIRD_PARTY_CHECKBOX="//label[contains(text(),'Contact is third-party')]";
     private final String CONTACT_NAME_FIELD="ContactName";
     private final String CONTACT_EMAILID="ContactEmailAddress";
+    private final String SELECT_CLI_FIELD_IS_REQUIRED="//span[contains(text(),'The Select CLI field is required.')]";
+    private final String THE_CONFIRM_POST_CODE_IS_REQUIRED="//span[contains(text(),'The Confirm Postcode field is required.')]";
+    private final String TEXT_ON_LINE_AND_INSTALLATION_DETAILS_PAGE="//legend[contains(text(),'Line and installation details')]";
+    private final String OBTAIN_ORDER_DETAILS="obtainOrderDetails";
+    private final String SELECT_CLI_DROPDOWN="//input[@id='CLI']/following-sibling::a";
+    private final String OPEN_REACH_SPINNER="//span[@class='spinner']";
+    private final String SEARCH_BOX_FOR_CLI="//input[@type='search']";
+    private final String SELECT_CLI_BUTTON="//button[contains(text(),'Select')]";
+    private final String CPS_PRESENT_RESULT="//div[@id='installationDetailsContainer']/div/h4/following-sibling::div/div[4]/div/label/strong/label";
+    private final String CALLING_AND_NETWORK_FEATURES="//div[@id='installationDetailsContainer']/div/h4/following-sibling::div/div[5]/div/label/strong/label";
+
 
 
     ElementUtils utils = new ElementUtils();
@@ -98,7 +112,7 @@ public class ServiceDesk_LoginIncidentPage {
         validationMessagesOnnTriagePage();
     }
 
-    public void openReachIncident_Triage() throws UnsupportedEncodingException, SQLException, ClassNotFoundException, InterruptedException {
+    public void openReachIncident_Triage(Boolean nextOrNot) throws UnsupportedEncodingException, SQLException, ClassNotFoundException, InterruptedException {
         utils.sqlExeQuery("portal", "MOE\\DEVSQL2008", "SN_aBILLity_Portal", "update incident_types set cli_type='1', openreach_function='1' where RecordID='4'");
         utils.sqlExeQuery("portal", "MOE\\DEVSQL2008", "SN_aBILLity_Portal", "update incident_symptoms set Symptom='Test Symptom 1', tmp_IncidentType='Test Type Fault 1' where RecordID='33'");
         utils.sqlExeQuery("portal", "MOE\\DEVSQL2008", "SN_aBILLity_Portal", "update incident_categories set category='Test Category 1',tmp_IncidentType='Test Type Fault 1' where RecordID='11'");
@@ -115,7 +129,8 @@ public class ServiceDesk_LoginIncidentPage {
         populateSummaryAndOwnerDropDown();
         clickNext();
         utils.waitForElementVisible(By.xpath("//a[@href='#tab3'][@aria-expanded='true']"));
-        clickPrevious();
+       if(nextOrNot==true)
+               clickPrevious();
     }
 
     public void populateSymptomAndCategory(String symptom, String category) {
@@ -175,5 +190,38 @@ public class ServiceDesk_LoginIncidentPage {
         clickNext();
         utils.sqlExeQuery("portal", "MOE\\DEVSQL2008", "SN_aBILLity_Portal", "update Defaultvalues set ValueNumber='0' where ID='150'");
     }
+    public void assertTextOnTicketDetailsPage(){
+        utils.waitForElementVisible(By.xpath(TEXT_ON_LINE_AND_INSTALLATION_DETAILS_PAGE));
+
+    }
+    public void clickObtainInstallationDetailsWithOutPopulatingCLIAndPostCode(){
+        utils.clickBtn(By.id(OBTAIN_ORDER_DETAILS));
+        utils.waitForElementVisible(By.xpath(SELECT_CLI_FIELD_IS_REQUIRED));
+        utils.waitForElementVisible(By.xpath(THE_CONFIRM_POST_CODE_IS_REQUIRED));
+    }
+    public void selectCLIToObtainInstallationDetails(String cli, String lineType, String numberOfLines, String maintenanceLevel ) throws InterruptedException {
+        utils.clickBtn(By.xpath(SELECT_CLI_DROPDOWN));
+        utils.waitForElementToVanish(By.xpath(OPEN_REACH_SPINNER));
+        utils.waitForElementVisible(By.xpath(SEARCH_BOX_FOR_CLI));
+        utils.sendText(By.xpath(SEARCH_BOX_FOR_CLI),""+cli+"");
+       Thread.sleep(1000);
+        utils.clickBtn(By.xpath(SELECT_CLI_BUTTON));
+        utils.waitForElementVisible(By.xpath("//h4[contains(text(),'Installation Details for "+cli+"')]"));
+        utils.waitForElementVisible(By.xpath("//strong[contains(text(),'"+lineType+"')]"));
+        utils.waitForElementVisible(By.xpath("//strong[contains(text(),'"+numberOfLines+"')]"));
+        utils.waitForElementVisible(By.xpath("//strong[contains(text(),'"+maintenanceLevel+"')]"));
+        utils.waitForElementVisibleForOpenReach(By.xpath(CPS_PRESENT_RESULT));
+        String valueForCPS=driver.findElement(By.xpath(CPS_PRESENT_RESULT)).getText();
+        String valueForCalAndNetwrkFeatures=driver.findElement(By.xpath(CALLING_AND_NETWORK_FEATURES)).getText();
+        utils.twoValueArrayList("Yes","No");
+        Assert.assertTrue(utils.allValues.contains(valueForCPS));
+        Assert.assertTrue(utils.allValues.contains(valueForCalAndNetwrkFeatures));
+
+
+
+
+
+    }
+
 }
 
---
+++ src/test/java/com/unionstreet/NxTier/stepDefs/SDSK/NA165_SDSK_Populate_OIDs_stepDefs.java
@@ -0,0 +1,33 @@
+package com.unionstreet.NxTier.stepDefs.SDSK;
+
+import com.unionstreet.NxTier.support.WebModel;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import java.io.UnsupportedEncodingException;
+import java.sql.SQLException;
+
+/**
+ * Created by RajeshG on 07/07/2017.
+ */
+public class NA165_SDSK_Populate_OIDs_stepDefs {
+    WebModel webModel=new WebModel();
+
+    NA160_SDSK_Login_Incident__Triage_stepDefs na160=new NA160_SDSK_Login_Incident__Triage_stepDefs();
+    @When("^I navigate to line and installation details of login in incident page$")
+    public void iNavigateToLineAndInstallationDetailsOfLoginInIncidentPage() throws InterruptedException, SQLException, ClassNotFoundException, UnsupportedEncodingException {
+        na160.iNavigateToTheTriagePageOfTheIncidentWizard();
+        webModel.getServiceDesk_loginIncidentPage().openReachIncident_Triage(false);
+        webModel.getServiceDesk_loginIncidentPage().assertTextOnTicketDetailsPage();
+        webModel.getServiceDesk_loginIncidentPage().clickObtainInstallationDetailsWithOutPopulatingCLIAndPostCode();
+
+
+
+
+    }
+
+    @Then("^I should be able to select a CLI and obtain its installation details$")
+    public void iShouldBeAbleToSelectACLIAndObtainItsInstallationDetails() throws InterruptedException {
+        webModel.getServiceDesk_loginIncidentPage().selectCLIToObtainInstallationDetails("01142733952","WLR3 ISDN 2e Standard","2","2");
+    }
+}
