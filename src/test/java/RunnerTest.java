85a0e62842faff427eedf2f033409e8cd9ab0622
UNIONST\rajeshg <rajesh.gurrala@unionstreet.uk.com>
2017-07-26 21:24:57 +0100

    tags seperated into @smokePack, @WhiteLabel, @WLR3, @SDSK clean test command being used

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -3,6 +3,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"html:target/whisper-html-report", "json:target/whisper_report.json", "com.cucumber.listener.ExtentCucumberFormatter:output/report.html"}, tags = {"@SDSK,@smokePack,@WLR3,@WhiteLabel"})
+@CucumberOptions(plugin = {"html:target/whisper-html-report", "json:target/whisper_report.json", "com.cucumber.listener.ExtentCucumberFormatter:output/report.html"},tags = {"@SDSK,@smokePack,@WhiteLabel,@WLR3"})
 public class RunnerTest {
 }
