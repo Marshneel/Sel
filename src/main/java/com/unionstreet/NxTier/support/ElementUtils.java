ebbb7ac21e24daf85d23a1315553b32a653ba9cb
UNIONST\rajeshg <rajesh.gurrala@unionstreet.uk.com>
2017-08-07 20:23:59 +0100

    tags seperated into @smokePack, @WhiteLabel, @WLR3, @SDSK clean test command being used

--- src/main/java/com/unionstreet/NxTier/support/ElementUtils.java
+++ src/main/java/com/unionstreet/NxTier/support/ElementUtils.java
@@ -186,18 +186,18 @@ public class ElementUtils {
 
     //browser selector
     public WebDriver browser() {
-     // String browser=System.getProperty("browser");
+     String browser=System.getProperty("browser");
         try {
 
-            if (getProperty("browser").equalsIgnoreCase("chrome")) {
+            if (browser.equalsIgnoreCase("chrome")) {
                 System.setProperty("webdriver.chrome.driver", "DriverFiles\\chromedriver.exe");
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.addArguments("--start-maximized");
                 driver = new ChromeDriver(chromeOptions);
-            } else if (getProperty("browser").equalsIgnoreCase("IE")) {
+            } else if (browser.equalsIgnoreCase("IE")) {
                 System.setProperty("webdriver.ie.driver", "DriverFiles\\IEDriverServer.exe");
                 driver = new InternetExplorerDriver();
-            } else if (getProperty("browser").equalsIgnoreCase("firefox")) {
+            } else if (browser.equalsIgnoreCase("firefox")) {
                 driver = new FirefoxDriver();}
 
         } catch (Exception e) {
