08b076f2c83974e20f08e9d9d28a7161d293c8f8
UNIONST\rajeshg <rajesh.gurrala@unionstreet.uk.com>
2017-08-30 10:57:21 +0100

    contains NA187

--- src/main/java/com/unionstreet/NxTier/pages/ServiceDesk_TicketDetailsPage.java
+++ src/main/java/com/unionstreet/NxTier/pages/ServiceDesk_TicketDetailsPage.java
@@ -452,9 +452,8 @@ public class ServiceDesk_TicketDetailsPage {
     clickBrowserIncidentsButton();
         }
         if (utils.isElementPresent(By.xpath("//div[@class='fade page-sidebar-fixed page-header-fixed page-container in page-sidebar-minified']")))
-        utils.clickBtn(By.xpath("//span[contains(text(),'Service Desk')]"));
-        utils.waitForElementVisible(By.xpath("//a[contains(text(),'Browse Incidents')]"));
-        utils.clickBtn(By.xpath("//a[contains(text(),'Browse Incidents')]"));
+       utils.clickBtn(By.xpath("//span[@class='sidebar-minify-btn']"));
+        clickBrowserIncidentsButton();
         {
     }}
     public void browserIncidents(String by, String commonSearch, String finalSearch) throws InterruptedException {
