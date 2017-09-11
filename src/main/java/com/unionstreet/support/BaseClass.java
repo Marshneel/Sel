d4bcf9d30cacd0f51e8d4896f6729812e3d213a3
UNIONST\rajeshg <rajesh.gurrala@unionstreet.uk.com>
2017-09-11 10:41:08 +0100

    contains NA187

--- src/main/java/com/unionstreet/support/BaseClass.java
+++ src/main/java/com/unionstreet/support/BaseClass.java
@@ -4,9 +4,12 @@ package com.unionstreet.support;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import java.io.IOException;
+
+import static org.openqa.selenium.OutputType.BYTES;
 
 public class BaseClass {
 
@@ -37,10 +40,10 @@ public class BaseClass {
     @After
     public void tearDown(Scenario scenario) throws IOException {
         if (scenario.isFailed()) {
-           utils.captureScreenShot(scenario.getName());
-//            TakesScreenshot camera = (TakesScreenshot) driver;
-//            byte[] screenshot = camera.getScreenshotAs(BYTES);
-//            scenario.embed(screenshot, "image/png");
+          // utils.captureScreenShot(scenario.getName());
+            TakesScreenshot camera = (TakesScreenshot) driver;
+            byte[] screenshot = camera.getScreenshotAs(BYTES);
+            scenario.embed(screenshot, "image/png");
        }
         driver.close();
         driver.quit();
